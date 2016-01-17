39798106bf5ccf0a3fccb6a0d2042bc43d68ab12
kalkin7 <kalkin7@gmail.com>
2016-01-17 23:03:37 +0900

    Initialize blog

---
+++ git_push.bat
@@ -0,0 +1,11 @@
+@echo off
+setlocal
+
+git pull origin gh-pages
+git add . -A
+set str=
+set /p str=Enter Commit Message:
+git commit -m "%str%"
+git push origin gh-pages
+cd ..
+purge_cloudflare_cache_kkwiki.bat
